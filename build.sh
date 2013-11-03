2137247029067dac2686853a78bc7e23ad95e764
DeanXu <xdx158@gmail.com>
2013-11-04 00:13:43 +0800

    update

---
+++ build.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+mkdir lib
+cd lib
+git clone https://github.com/mozilla/brick.git
+git clone https://github.com/maker/ratchet.git
+git clone https://github.com/FortAwesome/Font-Awesome.git
